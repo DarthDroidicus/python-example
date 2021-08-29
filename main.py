d22e15adbc8378ba026b1263956ea55da25c1c8c
Andres <crwolfwood@gmail.com>
2021-08-28 18:21:06 -0600

    some changes

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # This is a sample Python script.
 def main(msg):
     print(msg)
-print("Hello monkey king")
-print("Hello monkey king")
+print("Hello monkey pooper")
+print("Hello monkey scooper")
 print("Hello monkey king")
 main("Hello farts")
 
